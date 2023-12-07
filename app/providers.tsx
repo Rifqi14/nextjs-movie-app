ab39960e199b58c56ac9906151b1a7d37c3c3159
Muhammad Rifqi <rifqi.persie@gmail.com>
2023-12-08 00:11:34 +0700

    feat: add react-query providers in providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
 interface Props {
@@ -7,5 +8,10 @@ interface Props {
 }
 
 export function Providers({ children }: Props) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    <NextUIProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </NextUIProvider>
+  );
 }
